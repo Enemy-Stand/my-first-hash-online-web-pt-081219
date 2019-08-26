4746d1a200160896b56ba42f988e68b3fe643116
Joseph Sesti <sestijoey63@gmail.com>
2019-08-26 19:27:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,10 @@
 def my_hash
-  your_hash = {"my_first_language" => "Python", "my_second_language" => "Ruby"}
+  your_hash = {"my first_language" => "Python", "my second language" => "Ruby"}
 end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3
 end
 
 def retrieval
@@ -14,7 +14,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-shipping_manifest["key2"]
+shipping_manifest["oil paintings"]
 
 end
 
@@ -24,7 +24,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3,
                   "muskets" => 2,
-                  "gun_powder" 
+                  "gun powder" 
                  }
   # add 2 muskets to the shipping_manifest hash below
   # add 4 gun powder to the shipping_manifest hash below
